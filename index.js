fad55f94b5633909e829951ac7095ec3108bd314
nadia-prince <nadiamarde9@gmail.com>
2025-08-10 15:39:57 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import {startGame, getoldManNumber} from './functions.js'
-
-// אירוע טעינת הדף
-document.addEventListener('DOMContentLoaded', startGame);
-
-//אירוע לחיצה על כפתור
-document.querySelector('#btn').addEventListener('click', getoldManNumber);
